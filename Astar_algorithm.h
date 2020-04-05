4bc160b77e8752302b3adf3bf6c6489f7a0b8aaf
alexshakouri <acshakouri@ucdavis.edu>
2020-04-05 11:57:14 -0700

    Initial Implementation of A* algorithm

--- Astar_algorithm.h
+++ Astar_algorithm.h
@@ -67,7 +67,7 @@ public:
 	}
 
 	void calculate_cost_h(int goal_x, int goal_y){
-		//Diagonal Distance
+		//Diagonal Distance for 8 possible moves
 		this->cost_h = std::max( abs(this->position_x - goal_x), abs(this->position_y - goal_y));
 	}
 	void insert_parent(A_star_grid_info* parent){
@@ -89,6 +89,17 @@ A_star_grid_info minimum_cost_f(std::stack<A_star_grid_info> list){
 	
 }
 
+bool find_lower_cost_f(std::stack<A_star_grid_info> list, int successor_x, int successor_y, int successor_cost_f){
+	while(!list.empty()){
+		if((list.top()).get_position_x() == successor_x && (list.top()).get_position_y() == successor_y){
+			//TODO: Make sure that there can bever be multiple elements in the list that have the same (x,y)
+			return ((list.top()).get_cost_f() < successor_cost_f);
+		}
+		list.pop();
+	}
+	return false;
+}
+
 //TODO: Need to add grid here to update the map with the best route
 void Astar_algorithm(int start_x, int start_y, int goal_x, int goal_y){
 	
@@ -104,6 +115,7 @@ void Astar_algorithm(int start_x, int start_y, int goal_x, int goal_y){
 		//Find the minimum F in the open stack
 		current_position_info = minimum_cost_f(open_list);
 		//std::cout << current_position_info.get_cost_f() << std::endl;
+		//TODO::I NEED to fix the POP here (I'm not popping the right element
 		open_list.pop();
 
 		//Generate successor paths
@@ -115,6 +127,7 @@ void Astar_algorithm(int start_x, int start_y, int goal_x, int goal_y){
 				}
 				successor_x = x + current_position_info.get_position_x();
 				successor_y = y + current_position_info.get_position_y();
+				
 
 				//Skip parent node position
 				if(current_position_info.get_parent() != NULL){
@@ -131,9 +144,18 @@ void Astar_algorithm(int start_x, int start_y, int goal_x, int goal_y){
 				
 				A_star_grid_info successor_position_info(successor_x, successor_y);
 				successor_position_info.insert_parent(&current_position_info);
-				
+				std::cout << "succussor: " << successor_x << " " << successor_y << " parent: " << current_position_info.get_position_x() << " " << current_position_info.get_position_y() << " F: " << current_position_info.get_cost_f() << std::endl;
+
+
 				//Stop search if we are at the end
 				if(successor_x == goal_x && successor_y == goal_y){
+					while(successor_position_info.get_parent() != NULL){
+					std::cout << successor_position_info.get_position_x() <<  " : " << successor_position_info.get_position_y() << " F: " << successor_position_info.get_cost_f() << std::endl;
+					std::cout << successor_position_info.get_parent()->get_position_x() <<  " : " << successor_position_info.get_parent()->get_position_y() << " F: " << successor_position_info.get_parent()->get_cost_f() << std::endl;
+					std::cout << successor_position_info.get_parent()->get_parent()->get_position_x() <<  " : " << successor_position_info.get_parent()->get_parent()->get_position_y() << " F: " << successor_position_info.get_parent()->get_parent()->get_cost_f() << std::endl;
+					successor_position_info = *(successor_position_info.get_parent());
+					break;
+					}
 					return;
 				}
 
@@ -143,10 +165,21 @@ void Astar_algorithm(int start_x, int start_y, int goal_x, int goal_y){
 
 				successor_position_info.calculate_cost_f();
 				//std::cout << "F: " << successor_position_info.get_cost_f() << std::endl;
+				//std::cout << " pos:" << successor_position_info.get_position_x() << " " << successor_position_info.get_position_y() << std::endl;
 
+				if(find_lower_cost_f(open_list, successor_position_info.get_position_x(), successor_position_info.get_position_y(), successor_position_info.get_cost_f())){
+					continue;
+				}
+
+				if(find_lower_cost_f(closed_list, successor_position_info.get_position_x(), successor_position_info.get_position_y(), successor_position_info.get_cost_f())){
+					continue;
+				}
+
+				open_list.push(successor_position_info);
 			}
 		}
-		
+		//After going through all the successors push current node to closed_list
+		closed_list.push(current_position_info);
 	}
 	
 
